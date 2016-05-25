df2cd868a388be6bee012cdc305e1737828fba12
Jan-Wilem Buurlage <janwillembuurlage@gmail.com>
2016-05-25 23:43:16 +0200

    Add some inital structure

---
+++ include/graphics/window.hpp
@@ -0,0 +1,8 @@
+namespace tomovis {
+class window {
+    public:
+      window(server_);
+    private:
+      server& server_;
+};
+} // namespace tomovis
---
+++ include/server/server.hpp
@@ -0,0 +1,3 @@
+namespace tomovis {
+class server {};
+}
