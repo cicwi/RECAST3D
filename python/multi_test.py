775a76af3da0ac98f71678593f333d7fd8011694
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2017-06-26 17:14:13 +0200

    Update multi gpu script

--- python/multi_test.py
+++ python/multi_test.py
@@ -4,21 +4,24 @@ import threading
 
 class MiniGPUServer(threading.Thread):
     def __init__(self, group=None, target=None, name=None,
-             server=None, s=None, verbose=None):
-        threading.Thread.__init__(self, group=group, target=target, name=name,
-                                  verbose=verbose)
+             server=None, s=None):
+        threading.Thread.__init__(self, group=group, target=target, name=name)
+        print('init')
         self.server = server
         self.orientation_queue = []
-        self.event = threading.Event
+        self.event = threading.Event()
+        self.event.clear()
         self.s = s
         return
 
     def run(self):
+        print('run')
         while True:
-            event.wait()
+            self.event.wait()
+            print(self.s)
             # process orientations
             # and so on
-            event.clear()
+            self.event.clear()
 
 
 serv = tomop.server("hi")
@@ -36,12 +39,16 @@ p = 4
 # make p threads
 for i in range(p):
     threads.append(MiniGPUServer(server=serv, s=i))
+    threads[i].start()
 
 def callback(orientation, slice_id):
     global threads
+    print('callback')
     for thread in threads:
         thread.orientation_queue.append((orientation, slice_id))
-    thread.event.set()
+        # make sure that event is not yet set, otherwise wait?!
+        # alternative, use condition var
+        thread.event.set()
     return [], []
 
 serv.set_callback(callback)
