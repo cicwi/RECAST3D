776b97a38279ddb52cc33746b0a227c50dc8795e
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2018-10-12 23:57:08 +0200

    Add processing implementations

---
+++ src/util/processing.cpp
@@ -0,0 +1,91 @@
+#include <bulk/bulk.hpp>
+
+#include <algorithm>
+#include <cmath>
+#include <complex>
+#include <unsupported/Eigen/FFT>
+
+#include "slicerecon/util/processing.hpp"
+
+namespace slicerecon::util {
+
+void process_projection(bulk::world& world, int rows, int cols, float* data,
+                        const float* dark, const float* reciproc,
+                        const std::vector<float>& filter, int proj_id_min,
+                        int proj_id_max, bool weigh,
+                        const std::vector<float>& fdk_weights) {
+    // initialize fft
+    auto fft = Eigen::FFT<float>();
+    auto freq_buffer = std::vector<std::complex<float>>(cols, 0);
+
+    // divide work by rows
+    int s = world.rank();
+    int p = world.active_processors();
+    int block_size = ((rows - 1) / p) + 1;
+    int first_row = s * block_size;
+    int final_row = std::min((s + 1) * block_size, rows);
+
+    int count = proj_id_max - proj_id_min + 1;
+
+    for (int proj = 0; proj < count; ++proj) {
+        auto offset = proj * rows * cols;
+        for (auto r = first_row; r < final_row; ++r) {
+            int index = r * cols;
+            for (auto c = 0; c < cols; ++c) {
+                data[offset + index] =
+                    (data[offset + index] - dark[index]) * reciproc[index];
+                data[offset + index] = data[offset + index] <= 0.0f
+                                           ? 0.0f
+                                           : -std::log(data[offset + index]);
+                index++;
+            }
+
+            if (weigh) {
+                data[offset + index] *= fdk_weights[offset + index];
+            }
+
+            // filter the row
+            fft.fwd(freq_buffer.data(), &data[offset + r * cols], cols);
+            for (int i = 0; i < cols; ++i) {
+                freq_buffer[i] *= filter[i];
+            }
+            fft.inv(&data[offset + r * cols], freq_buffer.data(), cols);
+        }
+    }
+
+    world.barrier();
+}
+
+namespace filter {
+
+std::vector<float> ram_lak(int cols) {
+    auto result = std::vector<float>(cols);
+    auto mid = (cols + 1) / 2;
+    for (int i = 0; i < mid; ++i) {
+        result[i] = i;
+    }
+    for (int j = mid; j < cols; ++j) {
+        result[j] = 2 * mid - j;
+    }
+    return result;
+}
+
+std::vector<float> shepp_logan(int cols) {
+    auto result = std::vector<float>(cols);
+    auto mid = (cols + 1) / 2;
+
+    auto filter_weight = [=](auto i) {
+        return std::sin(M_PI * (i / (float)mid)) / (M_PI * (i / (float)mid));
+    };
+
+    for (int i = 1; i < mid; ++i) {
+        result[i] *= filter_weight(i);
+    }
+    for (int j = mid; j < cols; ++j) {
+        result[j] = filter_weight(2 * mid - j);
+    }
+    return result;
+}
+} // namespace filter
+
+} // namespace slicerecon::util
---
+++ src/util/util.cpp
@@ -0,0 +1,105 @@
+#include "slicerecon/util/util.hpp"
+
+namespace slicerecon::util {
+
+std::unique_ptr<astra::CParallelVecProjectionGeometry3D>
+proj_to_vec(astra::CParallelProjectionGeometry3D* parallel_geom) {
+    auto projs = parallel_geom->getProjectionCount();
+    auto angles = parallel_geom->getProjectionAngles();
+    auto rows = parallel_geom->getDetectorRowCount();
+    auto cols = parallel_geom->getDetectorColCount();
+    auto dx = parallel_geom->getDetectorSpacingX();
+    auto dy = parallel_geom->getDetectorSpacingY();
+
+    auto vectors = std::vector<astra::SPar3DProjection>(projs);
+    auto i = 0;
+    for (auto& v : vectors) {
+        auto vrd =
+            Eigen::Vector3f{std::sin(angles[i]), -std::cos(angles[i]), 0};
+        auto vc = Eigen::Vector3f{0, 0, 0};
+        auto vdx = Eigen::Vector3f{std::cos(angles[i]) * dx,
+                                   std::sin(angles[i]) * dx, 0};
+        auto vdy = Eigen::Vector3f{0, 0, dy};
+        vc -= 0.5f * ((float)rows * vdy + (float)cols * vdx);
+        v = {// ray dir
+             vrd[0], vrd[1], vrd[2],
+             // center
+             vc[0], vc[1], vc[2],
+             // pixel x dir
+             vdx[0], vdx[1], vdx[2],
+             // pixel y dir
+             vdy[0], vdy[1], vdy[2]};
+        ++i;
+    }
+
+    return std::make_unique<astra::CParallelVecProjectionGeometry3D>(
+        projs, rows, cols, vectors.data());
+}
+
+std::unique_ptr<astra::CConeVecProjectionGeometry3D>
+proj_to_vec(astra::CConeProjectionGeometry3D* cone_geom) {
+    auto projs = cone_geom->getProjectionCount();
+    auto angles = cone_geom->getProjectionAngles();
+    auto rows = cone_geom->getDetectorRowCount();
+    auto cols = cone_geom->getDetectorColCount();
+    auto dx = cone_geom->getDetectorSpacingX();
+    auto dy = cone_geom->getDetectorSpacingY();
+    auto dod = cone_geom->getOriginDetectorDistance();
+    auto dos = cone_geom->getOriginSourceDistance();
+
+    auto vectors = std::vector<astra::SConeProjection>(projs);
+    auto i = 0;
+    for (auto& v : vectors) {
+        auto vrd = Eigen::Vector3f{std::sin(angles[i]) * dos,
+                                   -std::cos(angles[i]) * dos, 0.0f};
+        auto vc = Eigen::Vector3f{-std::sin(angles[i]) * dod,
+                                  std::cos(angles[i]) * dod, 0.0f};
+        auto vdx = Eigen::Vector3f{std::cos(angles[i]) * dx,
+                                   std::sin(angles[i]) * dx, 0};
+        auto vdy = Eigen::Vector3f{0, 0, dy};
+        vc -= 0.5f * ((float)rows * vdy + (float)cols * vdx);
+
+        // ( srcX, srcY, srcZ, dX, dY, dZ, uX, uY, uZ, vX, vY, vZ )
+        v = {// source
+             vrd[0], vrd[1], vrd[2],
+             // center
+             vc[0], vc[1], vc[2],
+             // pixel x dir
+             vdx[0], vdx[1], vdx[2],
+             // pixel y dir
+             vdy[0], vdy[1], vdy[2]};
+        ++i;
+    }
+
+    return std::make_unique<astra::CConeVecProjectionGeometry3D>(
+        projs, rows, cols, vectors.data());
+}
+
+std::tuple<Eigen::Vector3f, Eigen::Matrix3f, Eigen::Vector3f>
+slice_transform(Eigen::Vector3f base, Eigen::Vector3f axis_1,
+                Eigen::Vector3f axis_2, float k) {
+    auto rotation_onto = [](Eigen::Vector3f x,
+                            Eigen::Vector3f y) -> Eigen::Matrix3f {
+        auto z = x.normalized();
+        auto w = y.normalized();
+        auto axis = z.cross(w);
+        if (axis.norm() < 0.0001f) {
+            return Eigen::Matrix3f::Identity();
+        }
+        auto alpha = z.dot(w);
+        auto angle = std::acos(alpha);
+        return Eigen::AngleAxis<float>(angle, axis.normalized()).matrix();
+    };
+
+    base = base * k;
+    axis_1 = axis_1 * k;
+    axis_2 = axis_2 * k;
+    auto delta = base + 0.5f * (axis_1 + axis_2);
+
+    auto rot = rotation_onto(axis_1, {2.0f * k, 0.0f, 0.0f});
+    rot = rotation_onto(rot * axis_2, {0.0f, 2.0f * k, 0.0f}) * rot;
+
+    return {-delta, rot, {1.0f, 1.0f, 1.0f}};
+}
+
+} // namespace slicerecon::util
