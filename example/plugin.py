5ffbd35834dd0910fae002f44f5d45616417be7f
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2018-10-23 16:18:54 +0200

    Add Tomos and Bulk as clean submodules

--- example/plugin.py
+++ example/plugin.py
@@ -1,14 +1,16 @@
 import numpy as np
 import slicerecon
 
-def callback(size, xs, idx):
-    xs = np.array(xs).reshape(size)
+
+def callback(shape, xs, idx):
+    xs = np.array(xs).reshape(shape)
 
     print("callback called", size)
     xs[xs <= 3.0] = 0.0
-    xs[xs > 3.0] = -10.0
+    xs[xs > 3.0] = 10.0
 
-    return [size, xs.ravel().tolist()]
+    return [shape, xs.ravel().tolist()]
+
 
 p = slicerecon.plugin("tcp://*:5652", "tcp://localhost:5555")
 p.set_slice_callback(callback)
