db3450eb2196e9f464211a9bf2761eabed7e5c83
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2020-03-19 10:13:41 +0100

    Update Otsu plugin

--- slicerecon/example/plugins/otsu_plugin.py
+++ slicerecon/example/plugins/otsu_plugin.py
@@ -3,14 +3,21 @@ import slicerecon
 from skimage import filters
 
 def callback(shape, xs, _):
-    # compute a threshold using Otsu's method
+    # The Otsu implementation does not accept only zeros, so we pass through
+    if not np.array(xs).any():
+        return [shape, xs]
+
+    # Reshape into an image
+    xs = np.array(xs).reshape(shape)
+
+    # Compute a threshold using Otsu's method
     val = filters.threshold_otsu(xs)
 
-    # threshold the image accordingly
+    # Threshold the image accordingly
     xs[xs <= val] = 0.0
     xs[xs > val] = 1.0
 
-    return [shape, xs]
+    return [shape, xs.ravel().tolist()]
 
 # Host a plugin on the default port, with the default RECAST3D endpoint
 p = slicerecon.plugin("tcp://*:5652", "tcp://localhost:5555")
