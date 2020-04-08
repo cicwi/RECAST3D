2a59b2ee04f6cdb73b6e2e49a8ee7bc5e1ace560
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2020-04-08 11:19:01 +0200

    Apply formatter to SliceRecon wrapper code

--- slicerecon/slicerecon/__init__.py
+++ slicerecon/slicerecon/__init__.py
@@ -8,14 +8,16 @@ __all__ = [
     "paganin_settings", "get_solver", "reconstruct_slice", "mute"
 ]
 
+
 def convert_acquisition_geometry(proj_geom, vol_geom):
     geom_type = proj_geom['type']
 
     if geom_type not in ['cone', 'cone_vec', 'parallel3d', 'parallel3d_vec']:
-        raise Exception("Only 3D cone-beam or parallel-beam geometries supported" )
+        raise Exception(
+            "Only 3D cone-beam or parallel-beam geometries supported")
 
     if geom_type == 'cone' or geom_type == 'parallel3d':
-       proj_geom =  astra.functions.geom_2vec(proj_geom)
+        proj_geom = astra.functions.geom_2vec(proj_geom)
 
     rows = proj_geom['DetectorRowCount']
     cols = proj_geom['DetectorColCount']
@@ -27,13 +29,29 @@ def convert_acquisition_geometry(proj_geom, vol_geom):
     print("proj_count", proj_count)
 
     opts = vol_geom['option']
-    volume_min_point = [opts['WindowMinX'], opts['WindowMinY'], opts['WindowMinZ']]
-    volume_max_point = [opts['WindowMaxX'], opts['WindowMaxY'], opts['WindowMaxZ']]
+    volume_min_point = [
+        opts['WindowMinX'], opts['WindowMinY'], opts['WindowMinZ']
+    ]
+    volume_max_point = [
+        opts['WindowMaxX'], opts['WindowMaxY'], opts['WindowMaxZ']
+    ]
 
-    return acquisition_geometry(rows=rows, cols=cols, proj_count=proj_count, angles=angles, parallel=parallel, vec_geometry=vec_geometry, volume_min_point=volume_min_point, volume_max_point=volume_max_point)
+    return acquisition_geometry(rows=rows,
+                                cols=cols,
+                                proj_count=proj_count,
+                                angles=angles,
+                                parallel=parallel,
+                                vec_geometry=vec_geometry,
+                                volume_min_point=volume_min_point,
+                                volume_max_point=volume_max_point)
 
 
-def get_solver(proj_geom, vol_geom, slice_size, proj_data, use_custom_filter=None, **kwargs):
+def get_solver(proj_geom,
+               vol_geom,
+               slice_size,
+               proj_data,
+               use_custom_filter=None,
+               **kwargs):
     parameters = settings(slice_size=slice_size, **kwargs)
     solver = reconstructor(parameters)
 
@@ -47,7 +65,8 @@ def get_solver(proj_geom, vol_geom, slice_size, proj_data, use_custom_filter=Non
     proj_data = np.transpose(proj_data, [1, 0, 2])
 
     for idx in range(proj_data.shape[0]):
-        push_projection(solver, idx, np.ascontiguousarray(proj_data[idx, :, :]))
+        push_projection(solver, idx,
+                        np.ascontiguousarray(proj_data[idx, :, :]))
 
     return solver
 
